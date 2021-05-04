543e0c148b0c1c9b7997900a130bc7d739761a87
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-05-04 10:25:11 +0300

    update, join, delete server.js

--- server.js
+++ server.js
@@ -62,6 +62,60 @@ app.get('/api/candidate/:id', (req, res) =>{
     })
 })
 
+//GET a party
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+
+    db.query(sql, (err, rows) =>{
+        if (err) {
+            res.status(500).json({error: err.message})
+            return
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+    })
+})
+
+//GET a specfic party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id]
+    db.query(sql, params, (err, row) =>{
+        if (err) {
+            res.status(500).json({error: err.message})
+            return
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+})
+
+//Delete a party
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`
+    const params = [req.params.id]
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({error: res.message})
+        } else if (!result.affectedRows){
+            res.json({
+                message: 'Party not found'
+            })
+        } else {
+            res.json ({
+                message: 'Deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            })
+        }
+    })
+})
+
 //Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`
